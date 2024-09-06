b385f8b4fae35ec3c7d0d44aca9c94faa98bf06b
ac-webdesign <ac.masterdesign@gmail.com>
2024-09-06 16:50:45 +0300

    first

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 // Set up Socket.io
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000", // Allow frontend to connect (adjust port as needed)
+    origin: "https://alex-chat.netlify.app/", // Allow frontend to connect
     methods: ["GET", "POST"]
   }
 });
@@ -42,6 +42,7 @@ io.on('connection', (socket) => {
 
 // Start the server
 const PORT = process.env.PORT || 5000;
+
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
